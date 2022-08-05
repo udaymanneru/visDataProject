ee957ad19725b2b2b622b02b4f30a59b55981a3c
udaymanneru <udaymanneru2005@gmail.com>
2022-08-05 09:29:30 -0400

    extra stuff

--- main.js
+++ main.js
@@ -3,20 +3,30 @@ let nameHeading = document.querySelector("h2");
 let image = document.querySelector("img");
 let information = document.querySelectorAll("#information-container p");
 let buttonID = document.querySelector("#randomButton")
+let copyClipboard = document.querySelector("#copytoclipboard");
+
+let globalName, globalAddress, globalEmail, globalAge, globalPhone;
 
 function populate(randIndex){
     person = data[randIndex];
     let name = person.name.first + " " + person.name.last;
+    console.log(name);
     nameHeading.innerHTML = "My name is " + name;
+    globalName = name;
 
     image.src = person.picture.large;
 
     let address = "Address: " + person.location.street.number + " " + person.location.street.name + " " + person.location.city + " " + " " + person.location.state + " " + person.location.country;
-    
-    let email = "Email: " + person.email;
-    let age = "Age: " + person.dob.age;
-    let phone = "Phone: " + person.phone;
+    globalAddress = address;
 
+    let email = "Email: " + person.email;
+    globalEmail = email;
+
+    let age = "Age: " + person.dob.age;
+    globalAge = age;
+
+    let phone = "Phone: " + person.phone;
+    globalPhone = phone;
 
     informationArr = [address, email, age, phone];
     for(let i = 0; i < informationArr.length; i++){
@@ -34,3 +44,9 @@ buttonID.onclick = function(){
     
 }
 
+
+copyClipboard.onclick = function(){
+    let information = globalName + "\n" + globalAddress + "\n" + globalEmail;
+    console.log(information);
+    navigator.clipboard.writeText(information);
+}
